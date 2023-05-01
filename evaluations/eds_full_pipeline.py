8c1ebbe3d0e83da1ee92df46aa2fc45eae0e7553
matthieu <m.doutreligne@has-sante.fr>
2023-05-01 19:50:35 +0200

    iter

--- evaluations/eds_full_pipeline.py
+++ evaluations/eds_full_pipeline.py
@@ -197,7 +197,7 @@ def create_parse_args_pipeline_evaluation():
     # Force the pretrain config to be the same as the one from [cehr_bert
     # README](https://github.com/cumc-dbmi/cehr-bert#3-pre-train-cehr-bert).
     setattr(pipeline_config, "epochs", 1)
-    setattr(pipeline_config, "batch_size", 128)
+    setattr(pipeline_config, "batch_size", 32)
     setattr(pipeline_config, "depth", 5)
     setattr(pipeline_config, "include_visit", True)
     setattr(pipeline_config, "max_seq_length", 512)
@@ -206,7 +206,7 @@ def create_parse_args_pipeline_evaluation():
     # I removed all required argument from the evaluation config.
     setattr(pipeline_config, "action", SEQUENCE_MODEL)
     # setattr(pipeline_config, "evaluation_depth", 5) # only used if using random bert
-    setattr(pipeline_config, "evaluation_batch_size", 32)
+    setattr(pipeline_config, "evaluation_batch_size", 128)
     setattr(pipeline_config, "evaluation_epochs", 10)
     setattr(pipeline_config, "model_evaluators", VANILLA_BERT_LSTM)
     return pipeline_config
