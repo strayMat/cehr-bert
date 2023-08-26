3d92e668e811f92ee7c3771bce2c6df7dc7957e1
matthieu <m.doutreligne@has-sante.fr>
2023-08-26 12:51:17 +0200

    prepare seed 3

--- evaluations/eds_prognosis_pipeline.py
+++ evaluations/eds_prognosis_pipeline.py
@@ -46,7 +46,7 @@ ICD10_CHAPTERS = [
 ]
 # ICD10_CHAPTERS = ["2"]
 
-GRID_RANDOM_SEED = list(range(0, 5))
+GRID_RANDOM_SEED = list(range(2, 3))
 GRID_PERCENTAGE = [0.5, 0.9, 0.99]  # [0.02, 0.1, 0.5, 1]
 
 PARAMETER_GRID = ParameterGrid(
